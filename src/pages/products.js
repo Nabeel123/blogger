a4da2ace56a8307253ecb973d92bd024b47461ea
Nabeel Francis <levisdevis66@gmail.com>
2020-03-23 06:09:45 -0700

    Product page ready

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,26 +1,54 @@
-// import React from "react"
-// // import { Link } from "gatsby"
-// import { graphql, StaticQuery, useStaticQuery } from "gatsby"
+import React from "react"
+// import { Link } from "gatsby"
+import { graphql, StaticQuery, useStaticQuery } from "gatsby"
 
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-
-
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 
-// class Products extends React.Component {
 
-//     componentDidMount() {
-//         fetch('https://jsonplaceholder.typicode.com/todos/1')
-//             .then(response => response.json())
-//             .then(json => console.log(json))
-//     }
 
-//     render() {
-//         <Layout>
-//             <SEO title="Products" />
-//             <h1> Products Page </h1>
-//         </Layout>
+class Products extends React.Component {
 
-//     };
-// } 
+    state = {
+        fetchedData: [],
+    };
+
+    componentDidMount() {
+        fetch('https://reqres.in/api/users')
+            .then(response => {
+                return response.json()
+            }).then(json =>
+                this.setState({
+                    fetchedData: json.data,
+                }));
+    }
+
+    render() {
+        const { fetchedData } = this.state
+        console.log(fetchedData);
+        return (
+            <Layout>
+                <SEO title="Products" />
+                <h1> Products Page </h1>
+                <div className="d-flex">
+
+                    {fetchedData.map(person =>
+
+                        <div key={person.id} className="card">
+                            <img src={person.avatar} />
+                            <p>{person.first_name}</p>
+                            <p>{person.email}</p>
+                        </div>
+
+                    )}
+
+                </div>
+            </Layout>
+        );
+
+    };
+}
+
+
+export default Products
